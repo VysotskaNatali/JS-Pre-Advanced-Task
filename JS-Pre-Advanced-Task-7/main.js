1eb03903dc72cb55939b3fe60ab9ea51b23af5ad
VysotskaNatali <Bljondinka7@gmail.com>
2022-07-02 00:05:58 +0300

    Search Movie

--- JS-Pre-Advanced-Task-7/main.js
+++ JS-Pre-Advanced-Task-7/main.js
@@ -1,87 +1,55 @@
 "use strict";
 const getS = (selector) => document.querySelector(selector);
 const getAl = (selector) => document.querySelectorAll(selector);
-const title = [],
-  rating = [],
-  genre = [],
-  rated = [],
-  year = [],
-  plot = [],
-  writer = [],
-  director = [],
-  actors = [],
-  boxOffice = [],
-  awards = [],
-  poster = [];
-  let check = 0;
 
-getS(".search-input").addEventListener("focus", function () {
+/* buttons form created */
+getS(".search-input").addEventListener("focus", function (event) {
   event.target.classList.add("focus");
 });
 
-getS(".search-input").addEventListener("input", function () {
+getS(".search-input").addEventListener("input", () => {
   getS(".search-close").classList.remove("hide");
-
 });
 
-getS(".search-input").addEventListener("blur", function () {
+getS(".search-input").addEventListener("blur", function (event) {
   if (this.value == "") {
     event.target.classList.remove("focus");
     getS(".search-close").classList.add("hide");
   }
 });
 
-getS(".search-close").addEventListener("click", function () {
+getS(".search-close").addEventListener("click", function (event) {
   getS(".search-input").value = "";
   event.target.classList.add("hide");
 });
 
+/*function html Search Movie */
 const getData = async () => {
   let movieSearch = getS(".search-input").value.toLowerCase();
-  const response = await fetch( `http://www.omdbapi.com/?s=${movieSearch}&apikey=d9d5408e`);
+  const response = await fetch(`http://www.omdbapi.com/?s=${movieSearch}&apikey=d9d5408e`);
   const data = await response.json();
   movieList(data.Search);
-  
- try {
+  try {
     for (let i = 0; i < data.Search.length; i++) {
-      const responseDetalies = await fetch( `http://www.omdbapi.com/?i=${data.Search[i].imdbID}&apikey=d9d5408e`);
+      const responseDetalies = await fetch(`http://www.omdbapi.com/?i=${data.Search[i].imdbID}&apikey=d9d5408e`);
       const dataDetalies = await responseDetalies.json();
-
-      title.push(dataDetalies.Title);
-      rating.push(dataDetalies.Ratings);
-      rated.push(dataDetalies.Rated);
-      year.push(dataDetalies.Year);
-      plot.push(dataDetalies.Plot);
-      writer.push(dataDetalies.Writer);
-      director.push(dataDetalies.Director);
-      actors.push(dataDetalies.Actors);
-      boxOffice.push(dataDetalies.BoxOffice);
-      awards.push(dataDetalies.Awards);
-      poster.push(dataDetalies.Poster);
-      genre.push(dataDetalies.Genre);
-     //console.log(dataDetalies);
-     detaliesMovie(data.Search);
+      getAl(".btn-movie")[i].addEventListener("click", function () {
+        detaliesMovie(dataDetalies);
+      });
     }
-    
- 
   } catch (err) {
     return console.log(err);
   }
 };
 
-// /* Search movie */
+/* Create Search Movie list */
 function movieList(arr) {
-  
-  getS(".movies-container").innerHTML = '';
- 
-  for (let i = 0; i < (arr.length+check); i++) {
-    createArr(arr[i])
+  getS(".movies-container").innerHTML = "";
+  for (let i = 0; i < arr.length; i++) {
+    createArr(arr[i]);
   }
-  
 }
-
-function createArr(movie){
-
+function createArr(movie) {
   getS(".movies-container").innerHTML += `<div class="movie">
       <div class="img-movie"><img src="${movie.Poster}"></div>
       <div class="name-movie">${movie.Title}</div>
@@ -89,40 +57,33 @@ function createArr(movie){
       <div class="year-movie">${movie.Year}</div>
       <input type="button" class="btn-movie" value="More detalies">
     </div>`;
-   
 }
 
 /* Movie Detalies show modal block */
-function detaliesMovie(arr) {
-  
-  for (let i = 0; i < arr.length; i++) {
-    getAl(".btn-movie")[i].addEventListener("click", function () {
-      getS(".detalis-container").classList.remove("hide");
-      getS(".title").textContent = `${title[i]}`;
-      getS(".detalis-img").style.backgroundImage = `url(${poster[i]})`;
-      getS(".rated").textContent = `${rated[i]}`;
-      getS(".year").textContent = `${year[i]}`;
-      getS(".genre").textContent = `${genre[i]}`;
-      getS(".writer").textContent = `${writer[i]}`;
-      getS(".director").textContent = `${director[i]}`;
-      getS(".actors").textContent = `${actors[i]}`;
-      getS(".boxOffice").textContent = `${boxOffice[i]}`;
-      getS(".awards").textContent = `${awards[i]}`;
-      getS(".plot").textContent = `${plot[i]}`;
-      for (let j = 0; j < rating[i].length; j++) {
-        getS(".ratings").innerHTML += `
-             <li>${rating[i][j].Source}&nbsp ${rating[i][j].Value}</li>`;
-      }
-    });
+async function detaliesMovie(arr) {
+  try {
+    getS(".detalis-container").classList.remove("hide");
+    getS(".title").textContent = arr.Title;
+    getS(".detalis-img").style.backgroundImage = `url("${arr.Poster}")`;
+    getS(".rated").textContent = arr.Rated;
+    getS(".year").textContent = arr.Year;
+    getS(".genre").textContent = arr.Genre;
+    getS(".writer").textContent = arr.Writer;
+    getS(".director").textContent = arr.Director;
+    getS(".actors").textContent = arr.Actors;
+    getS(".boxOffice").textContent = arr.BoxOffice;
+    getS(".awards").textContent = arr.Awards;
+    getS(".plot").textContent = arr.Plot;
+    for (let j = 0; j < arr.Ratings.length; j++) {
+      let li = document.createElement("li");
+      li.textContent += arr.Ratings[j].Source + " " + arr.Ratings[j].Value;
+      getS(".ratings").append(li);
+    }
+  } catch (err1) {
+    return console.log(err1);
   }
 }
-
-getS('.detalis-container').addEventListener('click',closeDetaliesMovie)
-
-function closeDetaliesMovie(){
-  
-    this.classList.add('hide');
-    
-}
-
-
+/* Close modal Movie Detalies */
+getS(".detalis-container").addEventListener("click", function () {
+  this.classList.add("hide");
+});
